24e2198a49873723deeed88610812369e4fffdde
Jotony Guerra <jotonyguerra@gmail.com>
2017-12-05 14:51:39 -0800

    working, edit, display,forms, delete,update ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
-  before_action :all_tasks, only: [:index, :create]
+  before_action :all_tasks, only: [:index, :create, :update]
+  before_action :set_tasks, only: [:edit, :update, :destroy]
   respond_to :html, :js
+
 
   def new
     @task = Task.new
@@ -10,10 +12,22 @@ class TasksController < ApplicationController
     @task  = Task.create(task_params)
   end
 
+  def update
+    @task.update_attributes(task_params)
+  end
+
+  def destroy
+   @task.destroy
+  end
+
   private
 
     def all_tasks
       @tasks = Task.all
+    end
+
+    def set_tasks
+      @task = Task.find(params[:id])
     end
 
     def task_params
